489c7b13f63c2b5dd6ce73b9b4e4ff8d4dff96da
GuntKritamate <kritamate.agis@mail.kmutt.ac.th>
2023-07-11 11:21:27 +0700

    update: homePage

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,23 @@
-
-
 const Home = () => {
   return (
-    <div>testing...</div>
-  )
-}
+    <section className="w-full flex-center flex-col">
+      {/* head_text is func in global.css file */}
+      <h1 className="head_text text-center">Discover & share</h1>
+      <br className="max-md:hidden" />
+      <span className="head_text orange_gradient text-center">
+        AI-powered prompt
+      </span>
+      <p className="desc text_center">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
